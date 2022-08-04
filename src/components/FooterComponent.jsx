1c53ea71cdfaf568bb684d3477186c73ae2c5cd2
Aayam <aayam.oza@gmail.com>
2022-08-04 17:15:58 +0545

    footer created. Icons and link to social medial

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,9 +1,21 @@
-import { Footer, Text } from "@mantine/core"
+import { Footer } from "@mantine/core";
+import { BrandInstagram, BrandYoutube, BrandTwitter } from "tabler-icons-react";
+import { Group, ActionIcon } from "@mantine/core";
 
 export default function CustomizedFooter() {
-    return (
-        <Footer height={60} p="md" sx={{ position: 'absolute', bottom: 0 }}>
-            <Text>Footer</Text>
-        </Footer>
-    )
+  return (
+    <Footer height={60} p="md" sx={{ position: "absolute", bottom: 0 }}>
+      <Group spacing="xs" position="center" noWrap>
+        <ActionIcon size="lg" variant="default" radius="xl">
+          <BrandTwitter size={18} />
+        </ActionIcon>
+        <ActionIcon size="lg" variant="default" radius="xl">
+          <BrandYoutube size={18} color={"white"} />
+        </ActionIcon>
+        <ActionIcon size="lg" variant="default" radius="xl">
+          <BrandInstagram size={18} />
+        </ActionIcon>
+      </Group>
+    </Footer>
+  );
 }
